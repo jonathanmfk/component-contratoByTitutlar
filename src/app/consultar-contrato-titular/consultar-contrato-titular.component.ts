320878d17c14397cf738ae47e26cdc23defa6052
jonathanmfk <jonathanmfk.23@gmail.com>
2021-09-23 13:12:43 -0500

    Initial commit

---
+++ src/app/consultar-contrato-titular/consultar-contrato-titular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-contrato-titular',
+  templateUrl: './consultar-contrato-titular.component.html',
+  styleUrls: ['./consultar-contrato-titular.component.css']
+})
+export class ConsultarContratoTitularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
